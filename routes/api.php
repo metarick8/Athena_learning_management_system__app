74ead4004960e22ea279c0f22776d27a1f1e7fe3
jad08 <jad88acc@gmail.com>
2025-09-13 17:24:11 +0300

    removed useless API's that my partner has made

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::post('/login', [UserController::class,'login']);
 */
 
 //
-Route::controller(UserController::class)->group(function(){
+Route::controller(UserController::class)->group(function () {
     Route::post('user/register', 'register');
     Route::post('login', 'login');
 })->middleware('auth:api');
@@ -50,38 +50,29 @@ Route::group(['middleware' => 'auth:api'], function () {
 
     Route::get('logout/{allDevices}', [UserController::class, 'logout']);
     //////////////////////Course///////////////////////////
-Route::get('/allCourses', [CourseController::class, 'getCourses']);
-Route::post('/addCourse', [CourseController::class, 'addCourse']);
-Route::delete('/delete/{id}', [CourseController::class, 'deleteCourse']);
-Route::get('/showCourse/{id}', [CourseController::class, 'showCourse']);
-Route::post('/search/{id}', [CourseController::class, 'findCourse']);///?
-/////////////////////Module////////////////////////////
-Route::post('/addModule', [ModuleController::class, 'addModule']);
-Route::post('/showModule/{id}', [ModuleController::class, 'showModule']);
-////////////////////Video/////////////////////////////
-Route::post('/addVideo', [VideoController::class, 'addVideo']);
-Route::get('/showVideo/{id}', [VideoController::class, 'showVideo']);
-///////////////////subscription///////////////////////
-Route::post('/subscribe', [SubscriptionController::class, 'subscribe']);
-///////////////////Rate///////////////////////////////
-Route::post('/addRate', [RateController::class, 'addRate']);
-Route::get('/getRate/{id}', [RateController::class, 'getRate']);
-//////////////////Follow/////////////////////////////
-Route::post('/follow/{id}', [FollowController::class, 'follow']);
-Route::post('/unfollow/{id}', [FollowController::class, 'unfollow']);
-Route::get('/getFollowers', [FollowController::class, 'getFollowers']);
-//////////////////Watch_Later////////////////////////
-Route::post('/addWishlist/{id}', [WatchLaterController::class, 'watch_later']);
-Route::get('/getWishlist', [WatchLaterController::class, 'getWatch']);
-/////////////////Book_Mark//////////////////////////
-Route::post('/addBookmark', [BookmarkController::class, 'addBookmark']);
-Route::post('/deleteBookmark', [BookmarkController::class, 'deleteBookmark']);
-Route::get('/getBookmarks', [BookmarkController::class, 'getBookmarks']);
-/////////////////Private_Course/////////////////////
-Route::post('/addPrivate', [PrivateCourseController::class, 'add_Private_Course']);
-/////////////////Private_Subscription///////////////
-Route::post('/PrivateSubscripe', [SubscriptionForPrivateController::class, 'Private_subscripe']);
-
-
+    Route::get('/allCourses', [CourseController::class, 'getCourses']);
+    Route::post('/addCourse', [CourseController::class, 'addCourse']);
+    Route::delete('/delete/{id}', [CourseController::class, 'deleteCourse']);
+    Route::get('/showCourse/{id}', [CourseController::class, 'showCourse']);
+    Route::post('/search/{id}', [CourseController::class, 'findCourse']); ///?
+    ///////////////////subscription///////////////////////
+    Route::post('/subscribe', [SubscriptionController::class, 'subscribe']);
+    ///////////////////Rate///////////////////////////////
+    Route::post('/addRate', [RateController::class, 'addRate']);
+    Route::get('/getRate/{id}', [RateController::class, 'getRate']);
+    //////////////////Follow/////////////////////////////
+    Route::post('/follow/{id}', [FollowController::class, 'follow']);
+    Route::post('/unfollow/{id}', [FollowController::class, 'unfollow']);
+    Route::get('/getFollowers', [FollowController::class, 'getFollowers']);
+    //////////////////Watch_Later////////////////////////
+    Route::post('/addWishlist/{id}', [WatchLaterController::class, 'watch_later']);
+    Route::get('/getWishlist', [WatchLaterController::class, 'getWatch']);
+    /////////////////Book_Mark//////////////////////////
+    Route::post('/addBookmark', [BookmarkController::class, 'addBookmark']);
+    Route::post('/deleteBookmark', [BookmarkController::class, 'deleteBookmark']);
+    Route::get('/getBookmarks', [BookmarkController::class, 'getBookmarks']);
+    /////////////////Private_Course/////////////////////
+    Route::post('/addPrivate', [PrivateCourseController::class, 'add_Private_Course']);
+    /////////////////Private_Subscription///////////////
+    Route::post('/PrivateSubscripe', [SubscriptionForPrivateController::class, 'Private_subscripe']);
 });
-
